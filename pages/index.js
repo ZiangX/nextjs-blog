2a6e66238aeac1a601156e739d84152910f863cc
Zuck Xu <ziangxuu@gmail.com>
2021-12-26 22:42:38 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
+  console.log('allPostsData', allPostsData);
   return (
     <Layout home>
       <Head>
@@ -15,34 +16,36 @@ export default function Home({ allPostsData }) {
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+          {Object.values(allPostsData).map((v) => {
+            console.log('val', v);
+            return (
+              <ul>
+                <li>{v.category_name_zh}</li>
+              </ul>
+            );
+          })}
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  // const allPostsData = getSortedPostsData()
+  let data = await fetch('https://modernfoodie.greenolivechurch.org/categories');
+  data = await data.json();
+
+  console.log(data.categories);
+
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData: data.categories,
+    },
+  };
 }
